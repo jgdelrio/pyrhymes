0d82321ca91939fc3d849ece1ed39c82cfeb58d4
Jose Del Rio <jose.sanz@reachplc.com>
2020-05-30 22:22:16 +0100

    NAS and more lyrics

--- pyrhymes/config.py
+++ pyrhymes/config.py
@@ -14,13 +14,13 @@ LOG_FOLDER = ROOT.joinpath('logs')
 # References
 BASE_URL = "https://www.azlyrics.com/"
 # Artists
-ARTISTS = ["k/kendricklamar.html", "n/nas.html", "n/notorious.html", "r/rakim.html"]
+ARTISTS = []
 ARTISTS_DONE = ["a/andre3000.html", "e/eminem.html", "g/ghostface.html", "h/hill.html", "i/icecube.html",
-                "j/jayz.html"]
+                "j/jayz.html", "k/kendricklamar.html", "n/nas.html", "n/notorious.html", "r/rakim.html"]
 
 FULL_URLS = [BASE_URL + art for art in ARTISTS]
 
 # Settings
-CRAWLER_WAIT = 16
+CRAWLER_WAIT = 17
 MAX_LENGTH = int(10000)
 VERBOSE = 1
